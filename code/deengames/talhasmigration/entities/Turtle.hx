0c936d2f16c4db50dc38bd5364406a0a5a52fead
ashes999 <ashes999@yahoo.com>
2017-07-04 21:32:19 -0400

    Switch to turtle image c/o IconFinder

--- code/deengames/talhasmigration/entities/Turtle.hx
+++ code/deengames/talhasmigration/entities/Turtle.hx
@@ -9,7 +9,10 @@ class Turtle extends Entity
     public function new()
     {
         super("player"); // tags are needed for collision
-        this.size(32, 32).colour(255, 0, 0).moveWithKeyboard(300)
+        this
+            //.size(32, 32).colour(255, 0, 0)
+            .image("assets/images/turtle.png")
+            .moveWithKeyboard(300)
             .velocity(0, 0).trackWithCamera().collideWith("ground");
     }
 }

73005b526d64fb27187195f6ddbce5f57bb808d3
ashes999 <ashes999@yahoo.com>
2017-07-23 23:37:45 -0400

    OpenFL -Dnext + ar-SA translation

--- code/source/Main.hx
+++ code/source/Main.hx
@@ -14,12 +14,12 @@ class Main extends Sprite
 	{
 		super();
 
-		var localizations = ["en-CA"];
+		var localizations = ["en-CA", "ar-SA"];
 		for (localization in localizations)
 		{
 			Translater.addLanguage(localization, Assets.getText('assets/data/localizations/${localization}.txt'));
 		}
-		Translater.selectLanguage(localizations[0]);
+		Translater.selectLanguage(localizations[1]);
 
 		addChild(new FlxGame(0, 0, deengames.talhasmigration.states.CoreGameState, 1, 60, 60, true));
 	}
